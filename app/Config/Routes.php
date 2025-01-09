699b4b209379924533f9f1a8f37e30ff4a8a554c
hansen671 <hansenindra17@gmail.com>
2025-01-09 18:51:10 +0700

    Update Admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/barang', 'Barang::index');
-$routes->get('/barang', 'Barang::index');
+
+// route admin dashboard
+$routes->get('dashboard', 'Admin\DashboardController::index');
